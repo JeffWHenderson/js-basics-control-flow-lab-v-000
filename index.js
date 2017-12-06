9e3022b749f553a000d9311406a75f75ddf73a33
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-06 04:17:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function scuberGreetingForFeet(distance) {
   if (distance < 400) {
     return "This one is on me!"
   } else if (distance > 2000) {
-    "I will gladly take your thirty bucks."
+    return "I will gladly take your thirty bucks."
   }
 }
