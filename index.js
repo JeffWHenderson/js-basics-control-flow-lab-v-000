27968dc47cd5a8a9a0dd580eccb4820e0816ccaa
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-06 04:27:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function switchOnCharmFromTip(amount) {
       return "Thank you so much.";
     case 'not as generous':
       return "Thank you.";
-    case 'thanks for everything'
+    default:
       return "Bye.";
   }
 }
